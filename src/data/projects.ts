59b4c623b633525c80649979e857f9f81fe2045e
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-10-05 20:41:12 +0200

    Chore: Added assets

---
+++ src/data/projects.ts
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    title: "Project 1",
+    subtitle: "Subtitle 1",
+    description: "Description 1",
+    image: "https://picsum.photos/200/300",
+    tags: ["tag1", "tag2", "tag3"],
+    link: "https://www.google.com",
+  },
+];
