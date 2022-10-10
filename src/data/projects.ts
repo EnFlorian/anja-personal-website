2254ddc893b8ac68960fd9b4ba96eed23a4f44d4
Enflorian <33962956+EnFlorian@users.noreply.github.com>
2022-10-10 05:23:41 +0200

    Fix: Fixed import

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ import project2 from "../assets/images/project2.jpg";
 import project3 from "../assets/images/project3.jpg";
 import project4 from "../assets/images/project4.jpg";
 import project5 from "../assets/images/project5.jpg";
-import project6 from "../assets/images/project6.jpg";
+import project6 from "../assets/images/project7.jpg";
 
 export const projects = [
   {
